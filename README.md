7328d8c6e2ae9e4b60cdce47c4108cab5e70da95
Alkhasar <franco.mostardi@gmail.com>
2022-02-12 10:27:13 +0100

    Base Commit (App Skeleton)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# TANS
+# PROGETTO TANS
 PET coincidence montecarlo simulation &amp; Noise equivalent count rate
+
+USEFUL LINKS:
+- Documenting C++ Code: https://developer.lsst.io/cpp/api-docs.html
+- Git Cheat Sheet: https://rogerdudler.github.io/git-guide/
---
+++ main.cpp
@@ -0,0 +1,12 @@
+/**
+ * Launches program from console
+ *
+ * @param argc Number of elements in argv array
+ * @param argv commands array
+ * 
+ * @returns execution state
+ */
+int main(int argc, char *argv[]){
+
+    return 0;   // Return 0 ("Succesful Execution") at the end of the program 
+}
